adcc4cd25d6fdbe8a5a86182045172e1729c38a9
XL1TTE <xl1tte.dev@gmail.com>
2026-04-02 08:52:13 +0700

    before merge

---
+++ ChessApp/lib/core/errors/validation_failure.dart
@@ -0,0 +1,20 @@
+import 'dart:collection';
+import 'package:chess/core/errors/failure.dart';
+
+final class ValidationErrors {
+  final HashMap<String, HashSet<String>> _errors = HashMap();
+  
+  void addError(String prop, String error) {
+    _errors[prop]?.add(error);
+    if(_errors.containsKey(prop) == false){
+      _errors[prop] = HashSet.from([error]);
+    }
+  }  
+  
+  Iterator<MapEntry<String, HashSet<String>>> get iterator => _errors.entries.iterator;
+}
+
+final class ValidationFailure extends Failure{
+  const ValidationFailure({required this.errors});
+  final ValidationErrors errors;
+}
--- ChessApp/lib/main.dart
+++ ChessApp/lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:chess/app.dart';
-import 'package:chess/application/queries/get_session/get_session_queary.dart';
-import 'package:chess/application/queries/get_session/get_session_queary_handler.dart';
 import 'package:chess/di/injection.dart';
 import 'package:flutter/material.dart';
-import 'package:streamline/streamline.dart';
 
 void main() {
   injectDependencies();
   runApp(
-    MediatorConfig(
-      queryHandlers: {
-        GetSessionQueary: () => getIt.get<GetSessionQuearyHandler>(),
-      },
-      commandHandlers: {},
-      child: const MainApp(),
-    ),
+    const MainApp()
   );
 }
 
